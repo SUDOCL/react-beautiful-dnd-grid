f5a3450a42d3176a983e5747e77fa33bce815ef2
SUDO <desarrollo@sudo.cl>
2022-12-13 18:29:13 -0300

    updated uuid library

--- src/ListManager.tsx
+++ src/ListManager.tsx
@@ -1,9 +1,9 @@
 import { DragAndDropWrapper } from "./DragAndDropWrapper/DragAndDropWrapper";
 import { withMaxItems } from "./withMaxItems/withMaxItems";
 import { withReactToItemsChange } from "./withReactToItemsChange/withReactToItemsChange";
-import uuid from "uuid/v4";
+import { v4 as uuidv4 } from 'uuid';
 
-const ComponentWithMaxItems = withMaxItems(DragAndDropWrapper, uuid);
+const ComponentWithMaxItems = withMaxItems(DragAndDropWrapper, uuidv4);
 const ComponentWithReactToItemsChange = withReactToItemsChange(ComponentWithMaxItems);
 
 export const ListManager = ComponentWithReactToItemsChange;
